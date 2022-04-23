891f24f922d6dfcae677073103f6a21511dd3410
Schifferking <schifferking@hotmail.com>
2022-04-23 14:32:26 -0500

    Create operate() function
    The operate() function has three arguments: the operator that determines
    the operation to be performed, and the two numbers that will be used.
    
    It has a switch statement to deal with the choices and invoke the
    previous functions when the operator is identified.

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
+function operate(operator, num1, num2) {
+  switch(operator) {
+    case '+':
+      add(num1, num2);
+    break;
+
+    case '-':
+      substract(num1, num2);
+    break;
+
+    case '*':
+      multiply(num1, num2);
+    break;
+
+    case '/':
+      divide(num1, num2);
+    break;
+  }
+}
+
 function add(num1, num2) {
-    return num1 + num2;
+  return num1 + num2;
 }
 
 function substract(num1, num2) {
-    return num1 - num2;
+  return num1 - num2;
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+  return num1 * num2;
 }
 
 function divide(num1, num2) {
-    if(num2 === 0){
-        return "Error, divide by 0";
-    }
-    return num1 / num2;
+  if(num2 === 0){
+    return "Error, divide by 0";
+  }
+  return num1 / num2;
 }
