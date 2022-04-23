63b978d5e88a0c59925433b147704ee7c0adfdf6
Schifferking <schifferking@hotmail.com>
2022-04-23 14:20:31 -0500

    Add basic arithmetic functions
    These functions will be used later with another one and then with
    buttons.

---
+++ script.js
@@ -0,0 +1,18 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function substract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    if(num2 === 0){
+        return "Error, divide by 0";
+    }
+    return num1 / num2;
+}
